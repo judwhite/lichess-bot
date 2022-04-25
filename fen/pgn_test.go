09bef9f704d2fb5860f570e3f2788499f07e21fd
Jud White <jud.white@blueskysystems.com>
2022-04-24 23:28:23 -0500

    BenchmarkPGNtoMoves: extract one pgn for benchmarking

--- fen/pgn_test.go
+++ fen/pgn_test.go
@@ -71,13 +71,14 @@ func pgnMovesTestData(tb testing.TB) []PGNMoves {
 }
 
 func BenchmarkPGNtoMoves(b *testing.B) {
-	cases := pgnMovesTestData(b)
-	if len(cases) == 0 {
-		b.Fatal("no test data")
-	}
-	pgn := cases[0].PGN
-
-	b.ResetTimer()
+	const pgn = `1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 4. O-O Nxe4 5. d4 Nd6 6. Bxc6 dxc6 7. dxe5
+Nf5 8. Qxd8+ Kxd8 9. Nc3 Be7 10. Bf4 Be6 11. g4 Nh4 12. Nxh4 Bxh4 13. g5
+Ke7 14. Ne4 Rhd8 15. Rfe1 a5 16. a4 b6 17. Kg2 Rd4 18. c3 Rd3 19. Bg3 Bxg3
+20. hxg3 Bg4 21. f4 c5 22. Kg1 Rad8 23. Rac1 c4 24. Nf2 Rxg3+ 25. Kh2 Rf3
+26. Nxg4 Rd2+ 27. Kg1 Rg3+ 28. Kf1 Rxg4 29. Re4 Rgg2 30. Rxc4 Rdf2+ 31. Ke1
+Re2+ 32. Kf1 Ref2+ 33. Ke1 Rxb2 34. Kf1 Rbf2+ 35. Ke1 Ra2 36. Kf1 Raf2+ 37.
+Ke1 Rh2 38. Rxc7+ Kd8 39. Rc4 Rb2 40. Rd1+ Ke7 41. Rc7+ Ke6 42. Rd6+ Kf5
+43. Rd2 Rbxd2 44. Rxf7+ Kg4 45. Rf6 Ra2 46. Rxb6 Rh1# 0-1`
 
 	for i := 0; i < b.N; i++ {
 		_, err := PGNtoMoves(pgn)
